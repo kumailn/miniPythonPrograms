7589c74a90cfcfe2b611323834971ffec5688dde
Kumail <kumailmn@gmail.com>
2017-07-04 12:22:39 -0400

    two

--- RiemannZetaCoprime.py
+++ RiemannZetaCoprime.py
@@ -1,4 +1,4 @@
-#Exprimentally calculates the Riemann Zeta function at 3 and 4 using Eulers coprime conjecture 
+#Exprimentally calculates the Riemann Zeta function at 2, 3 and 4 using Eulers coprime conjecture 
 from math import gcd as bltin_gcd
 from random import randint
 
@@ -9,7 +9,17 @@ def coprime2(a, b, c):
 def coprime3(a, b, c, d):
     return bltin_gcd(d, bltin_gcd(c, bltin_gcd(a, b))) == 1
 
-
+def cns1(s):
+    l = []
+    for i in range(s):
+        l += [[randint(0, 10000), randint(0, 10000)]]
+    c = 0
+    for i in range(len(l)):
+        if bltin_gcd(l[i][0], l[i][1]) == True:
+            c += 1
+    l2 = [len(l), c, len(l)/c]
+    #print(l)
+    return l2
 
 def cns(s):
     l = []
@@ -20,6 +30,7 @@ def cns(s):
         if coprime2(l[i][0], l[i][1], l[i][2]) == True:
             c += 1
     l2 = [len(l), c, len(l)/c]
+    print(l)
     return l2
 
 def cns2(s):
