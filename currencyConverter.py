24dabbf064eb24ada4cee318af466072d0c24979
Kumail <kumailmn@gmail.com>
2017-07-27 20:42:15 -0400

    update

--- currencyConverter.py
+++ currencyConverter.py
@@ -6,4 +6,9 @@ def toUSD(a):
     #Converts CAD to USD
     return(cc.convert("CAD", "USD", Decimal(a)))
 
+
+def toCAD(a):
+    #Converts USD to CAD
+    return(cc.convert("USD", "CAD", Decimal(a)))
+
 print(toUSD(170))
